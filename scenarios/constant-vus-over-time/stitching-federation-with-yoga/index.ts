af7f492f984d20938a2bb52f497a390ab44a4f4f
Dotan Simha <dotansimha@gmail.com>
2023-04-25 13:45:50 +0300

    added stitching-federation-with-yoga

---
+++ scenarios/constant-vus-over-time/stitching-federation-with-yoga/index.ts
@@ -0,0 +1,30 @@
+import { createServer } from "node:http";
+import { createYoga } from "graphql-yoga";
+import { getStitchedSchemaWithUrls } from "@graphql-tools/federation";
+
+async function main() {
+  const schema = await getStitchedSchemaWithUrls([
+    {
+      endpoint: "http://accounts:4001/graphql",
+    },
+    {
+      endpoint: "http://reviews:4004/graphql",
+    },
+    {
+      endpoint: "http://products:4003/graphql",
+    },
+    {
+      endpoint: "http://inventory:4002/graphql",
+    },
+  ]);
+
+  const yoga = createYoga({ schema });
+  const server = createServer(yoga);
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 4000;
+
+  server.listen(port, () => {
+    console.info(`Server is running on http://localhost:${port}/graphql`);
+  });
+}
+
+main().catch(console.error);
