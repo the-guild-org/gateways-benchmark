ebffa977c515f5aaf41b0d77eede8666d44889c0
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-04-27 14:45:18 +0300

    Fix

--- scenarios/constant-vus-over-time/stitching-federation-with-yoga/index.ts
+++ scenarios/constant-vus-over-time/stitching-federation-with-yoga/index.ts
@@ -2,29 +2,25 @@ import { createServer } from "node:http";
 import { createYoga } from "graphql-yoga";
 import { getStitchedSchemaWithUrls } from "@graphql-tools/federation";
 
-function main() {
-  const schema = getStitchedSchemaWithUrls([
-    {
-      endpoint: "http://accounts:4001/graphql",
-    },
-    {
-      endpoint: "http://reviews:4004/graphql",
-    },
-    {
-      endpoint: "http://products:4003/graphql",
-    },
-    {
-      endpoint: "http://inventory:4002/graphql",
-    },
-  ]);
+const schema = getStitchedSchemaWithUrls([
+  {
+    endpoint: "http://accounts:4001/graphql",
+  },
+  {
+    endpoint: "http://reviews:4004/graphql",
+  },
+  {
+    endpoint: "http://products:4003/graphql",
+  },
+  {
+    endpoint: "http://inventory:4002/graphql",
+  },
+]);
 
-  const yoga = createYoga({ schema });
-  const server = createServer(yoga);
-  const port = process.env.PORT ? parseInt(process.env.PORT) : 4000;
+const yoga = createYoga({ schema });
+const server = createServer(yoga);
+const port = process.env.PORT ? parseInt(process.env.PORT) : 4000;
 
-  server.listen(port, () => {
-    console.info(`Server is running on http://localhost:${port}/graphql`);
-  });
-}
-
-main().catch(console.error);
+server.listen(port, () => {
+  console.info(`Server is running on http://localhost:${port}/graphql`);
+});
