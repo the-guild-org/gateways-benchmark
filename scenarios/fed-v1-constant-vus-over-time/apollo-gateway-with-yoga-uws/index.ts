61ad86b82f4fb4547293ae33473da106202b5f0d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-08-01 12:38:40 +0300

    Seperate Yoga uWS and Yoga node:http

--- scenarios/fed-v1-constant-vus-over-time/apollo-gateway-with-yoga-uws/index.ts
+++ scenarios/fed-v1-constant-vus-over-time/apollo-gateway-with-yoga-uws/index.ts
@@ -1,4 +1,4 @@
-import { createServer } from 'node:http';
+import { App } from "uWebSockets.js";
 import { createYoga } from "graphql-yoga";
 import { ApolloGateway } from "@apollo/gateway";
 import { readFileSync } from "fs";
@@ -12,10 +12,10 @@ async function main() {
   const yoga = createYoga({
     plugins: [useApolloFederation({ gateway })],
   });
-  const server = createServer(yoga);
+  const server = App().any("/*", yoga);
   const port = process.env.PORT ? parseInt(process.env.PORT) : 4000;
 
-  server.listen(port, () => {
+  server.listen('0.0.0.0', port, () => {
     console.info(`Server is running on http://localhost:${port}/graphql`);
   });
 }
