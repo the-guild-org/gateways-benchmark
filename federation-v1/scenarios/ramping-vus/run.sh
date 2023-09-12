1e6411eb4ddc75343095ffe156b6a7431b5e1997
Dotan Simha <dotansimha@gmail.com>
2023-09-12 13:34:47 +0300

    fix

--- federation-v1/scenarios/ramping-vus/run.sh
+++ federation-v1/scenarios/ramping-vus/run.sh
@@ -6,55 +6,53 @@ if [ ! -n "$1" ]; then
   exit 1
 fi
 
-# needed because we are taking containers from another directory, and Docker can be stupid sometimes
+export BASE_DIR=$( realpath ./ )
 
-export BASE_DIR=$( realpath ../fed-v1-constant-vus-over-time )
+export LOCAL_ENV_OVERRIDE=""
+
+if [[ -z "${CI}" ]]; then
+    export LOCAL_ENV_OVERRIDE="-f ../../subgraphs/docker-compose.subgraphs.local.yaml"
+fi
+
+export OUT_DIR="../../gateways/$1"
+export COMPOSE_FLAGS="-f ../../../docker-compose.metrics.yaml -f ../../subgraphs/docker-compose.subgraphs.yaml $LOCAL_ENV_OVERRIDE -f $OUT_DIR/docker-compose.yaml"
 
 on_error(){
     cd $BASE_DIR
     docker inspect gateway
     docker logs gateway
-    docker compose  -f ../../docker-compose.metrics.yaml  -f ../fed-v1-constant-vus-over-time/docker-compose.services.yaml -f ../fed-v1-constant-vus-over-time/$1/docker-compose.yaml ps
-    docker compose  -f ../../docker-compose.metrics.yaml  -f ../fed-v1-constant-vus-over-time/docker-compose.services.yaml -f ../fed-v1-constant-vus-over-time/$1/docker-compose.yaml logs
+    docker compose $COMPOSE_FLAGS ps
+    docker compose $COMPOSE_FLAGS logs
 }
  
 trap 'on_error' ERR
 
-docker compose  -f ../../docker-compose.metrics.yaml  -f ../fed-v1-constant-vus-over-time/docker-compose.services.yaml -f ../fed-v1-constant-vus-over-time/$1/docker-compose.yaml up -d --wait --force-recreate
+docker compose $COMPOSE_FLAGS up -d --wait --force-recreate
 
 if [[ -z "${CI}" ]]; then
-    trap "docker compose  -f ../../docker-compose.metrics.yaml  -f ../fed-v1-constant-vus-over-time/docker-compose.services.yaml -f ../fed-v1-constant-vus-over-time/$1/docker-compose.yaml down && exit 0" INT
+    trap "docker compose $COMPOSE_FLAGS down && exit 0" INT
 fi
 
 export K6_PROMETHEUS_RW_SERVER_URL=http://localhost:9090/api/v1/write
-
 export K6_PROMETHEUS_RW_TREND_AS_NATIVE_HISTOGRAM=true
-
 export START_TIME="$(date +%s)"
 
-mkdir ./$1 || echo ""
+k6 --out=experimental-prometheus-rw --out json=$OUT_DIR/k6_metrics.json run -e SUMMARY_PATH="$OUT_DIR" benchmark.k6.js
 
-k6 --out=experimental-prometheus-rw --out json=./$1/k6_metrics.json run -e SUMMARY_PATH="./$1" benchmark.k6.js
-
-# sleep for longer, to allow us to measure the cooldown period and see if RAM/CPU usage drops
-
-sleep 10
+sleep 2
 
 export END_TIME="$(date +%s)"
 
-docker logs gateway > ./$1/gateway_log.txt
+docker logs gateway > $OUT_DIR/gateway_log.txt
 
-rm -rf ./$1/overview.png || echo ""
+rm -rf $OUT_DIR/overview.png || echo ""
+npx --quiet capture-website-cli "http://localhost:3000/d/01npcT44k/k6?orgId=1&from=${START_TIME}000&to=${END_TIME}000&kiosk" --output $OUT_DIR/overview.png --width 1200 --height 740
 
-npx --quiet capture-website-cli "http://localhost:3000/d/01npcT44k/k6?orgId=1&from=${START_TIME}000&to=${END_TIME}000&kiosk" --output ./$1/overview.png --width 1200 --height 740
+rm -rf $OUT_DIR/http.png || echo ""
+npx --quiet capture-website-cli "http://localhost:3000/d-solo/01npcT44k/k6?orgId=1&from=${START_TIME}000&to=${END_TIME}000&panelId=41" --output $OUT_DIR/http.png --width 1200
 
-rm -rf ./$1/http.png || echo ""
-
-npx --quiet capture-website-cli "http://localhost:3000/d-solo/01npcT44k/k6?orgId=1&from=${START_TIME}000&to=${END_TIME}000&panelId=41" --output ./$1/http.png --width 1200
-
-rm -rf ./$1/containers.png || echo ""
-
-npx --quiet capture-website-cli "http://localhost:3000/d/pMEd7m0Mz/cadvisor-exporter?orgId=1&var-host=All&var-container=accounts&var-container=inventory&var-container=products&var-container=reviews&from=${START_TIME}000&to=${END_TIME}000&kiosk" --output ./$1/containers.png --width 1200
+rm -rf $OUT_DIR/containers.png || echo ""
+npx --quiet capture-website-cli "http://localhost:3000/d/pMEd7m0Mz/cadvisor-exporter?orgId=1&var-host=All&var-container=accounts&var-container=inventory&var-container=products&var-container=reviews&from=${START_TIME}000&to=${END_TIME}000&kiosk" --output $OUT_DIR/containers.png --width 1200
 
 if [[ -z "${CI}" ]]; then
     echo "Done, you can find some stats in Grafana: http://localhost:3000/d/01npcT44k/k6?orgId=1&from=${START_TIME}000&to=${END_TIME}000"
