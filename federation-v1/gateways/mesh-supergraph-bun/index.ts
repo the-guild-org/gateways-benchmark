e108f14c912b511e0946def49eecfc675ba3c05b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-10-21 14:48:57 +0300

    Try bun

--- federation-v1/gateways/mesh-supergraph-bun/index.ts
+++ federation-v1/gateways/mesh-supergraph-bun/index.ts
@@ -1,8 +1,10 @@
-import { createBuiltMeshHTTPHandler } from './.mesh'
+import { createGatewayRuntime } from '@graphql-hive/gateway';
 
 const server = Bun.serve({
     port: Bun.env.PORT ? parseInt(Bun.env.PORT) : 4000,
-    fetch: createBuiltMeshHTTPHandler(),
+    fetch: (createGatewayRuntime({
+        supergraph: () => Bun.file('./supergraph.graphql').text(),
+    }) as any),
 })
 
 console.info(`Server is running on http://localhost:${server.port}/graphql`);
