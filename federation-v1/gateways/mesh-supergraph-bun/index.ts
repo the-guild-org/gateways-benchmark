749dc8e5276457892f2aea367a2256525309b75d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-10-21 14:50:28 +0300

    Enable JIT

--- federation-v1/gateways/mesh-supergraph-bun/index.ts
+++ federation-v1/gateways/mesh-supergraph-bun/index.ts
@@ -1,10 +1,14 @@
 import { createGatewayRuntime } from '@graphql-hive/gateway';
+import { useJIT } from '@graphql-mesh/plugin-jit';
 
 const server = Bun.serve({
     port: Bun.env.PORT ? parseInt(Bun.env.PORT) : 4000,
-    fetch: (createGatewayRuntime({
+    fetch: (createGatewayRuntime as any)({
         supergraph: () => Bun.file('./supergraph.graphql').text(),
-    }) as any),
+        plugins: () => [
+            useJIT(),
+        ]
+    }),
 })
 
 console.info(`Server is running on http://localhost:${server.port}/graphql`);
