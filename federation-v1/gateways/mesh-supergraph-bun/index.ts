6f8bef1ac20fcd73d855f6f1ad037735fdb7b1b9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-10-22 18:08:36 +0300

    Cleanup unused gateways (#479)
    * Cleanup Gateways
    
    * Healthcheck
    
    * ...
    
    * Better healthcheck
    
    * Another attempt
    
    * Disable grafbase for now

--- federation-v1/gateways/mesh-supergraph-bun/index.ts
+++
@@ -1,8 +0,0 @@
-import { createBuiltMeshHTTPHandler } from './.mesh'
-
-const server = Bun.serve({
-    port: Bun.env.PORT ? parseInt(Bun.env.PORT) : 4000,
-    fetch: createBuiltMeshHTTPHandler(),
-})
-
-console.info(`Server is running on http://localhost:${server.port}/graphql`);
